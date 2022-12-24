ad0158b8e3eb2196f2063b322a91559ca38efdf4
Vu Le Bao Long <vulebaolong@gmail.com>
2022-12-24 09:57:44 +0700

    repons nav 24-12

--- js/index.js
+++ js/index.js
@@ -1,51 +1,37 @@
 'use-strict'
 
-// const mainEl = document.querySelector('main')
-// const hero_sectionEl = document.querySelector('.hero_section')
-// const headerEl = document.querySelector('.header')
+const classArr = [
+    'hero_section',
+    'header',
+    'header_nav',
+    'header_nav_mobile-1-close',
+    'icon_nav_open'
+]
+const element = {}
 
-// const options = {
-//     root: null,
-//     rootMargin: '0px',
-//     threshold: 1
-// }
-// const handler = (entries) => {
-//     console.log(entries)
-//     console.log(entries[0].isIntersecting)
-//     console.log(entries[0].boundingClientRect.y)
-//     // entries is an array of observed dom nodes
-//     // we're only interested in the first one at [0]
-//     // because that's our .sentinal node.
-//     // Here observe whether or not that node is in the viewport
-//     if (entries[0].isIntersecting ) {
-//         headerEl.classList.remove('sticky')
-//         console.log('remove');
-//     } else {
-//         headerEl.classList.add('sticky')
-//         console.log('add');
-//     }
-// }
+for (const item of classArr) {
+    console.log(item);
+    element[`${item}El`] = document.querySelector(`.${item}`)
+}
 
-// // create the observer
-// const observer = new window.IntersectionObserver(handler, options)
-// // give the observer some dom nodes to keep an eye on
-// observer.observe(hero_sectionEl)
-
-
-const hero_sectionEl = document.querySelector('.hero_section')
-const headerEl = document.querySelector('.header')
 
 function handler(scrollPos) {
     window_scrollY = window.scrollY;
-    console.log(window_scrollY);
+    // console.log(window_scrollY);
     if (window_scrollY) {
         // console.log('thêm');
-        headerEl.classList.add('sticky')
+        element[`headerEl`].classList.add('sticky')
     } else {
         // console.log('xóa');
-        headerEl.classList.remove('sticky')
+        element[`headerEl`].classList.remove('sticky')
     }
 
 }
 
+function openCloseNav(params) {
+    element[`header_navEl`].classList.toggle('close')
+}
+
 document.addEventListener("scroll", handler);
+element[`icon_nav_openEl`].addEventListener("click", openCloseNav);
+element[`header_nav_mobile-1-closeEl`].addEventListener("click", openCloseNav);
